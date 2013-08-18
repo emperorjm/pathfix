25e55a1c4123c8d0e3d87f4203abc5fa848266f3
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2013-08-18 00:16:49 -0400

    Relative anchor link paths are now converted to a drupalgap_goto() call.

--- pathauto.js
+++ pathauto.js
@@ -9,13 +9,28 @@ function pathauto_entity_post_render_content(entity) {
     var replacement = "$1" + drupalgap.settings.site_path + "$2";
     entity.content = entity.content.replace(regex, replacement);
     
+    // Replace internal links with drupalgap_goto() calls.
+    // kodos syntax backup: href=['"]/([-A-Za-z0-9+&@#\/%?=~_()|!:,.;]*[-A-Za-z0-9+&@#\/%=~_()|])['"]
+    regex = /href=['"]\/([-A-Za-z0-9+&@#\/%?=~_()|!:,.;]*[-A-Za-z0-9+&@#\/%=~_()|])['"]/g;
+    var matches = entity.content.match(regex);
+    if (matches && matches.length) {
+      $.each(matches, function(index, match){
+          replacement = 'onclick="javasciprt:drupalgap_goto(\'' + match.substring(7, match.length-1) + '\');"';
+          entity.content = entity.content.replace(match, replacement);
+      });
+    }
+    
     // Replace external links with an InAppBrowser link.
     // credit: http://www.codinghorror.com/blog/2008/10/the-problem-with-urls.html
     // kodos syntax backup: href=["']\(?\bhttp://[-A-Za-z0-9+&@#/%?=~_()|!:,.;]*[-A-Za-z0-9+&@#/%=~_()|]['"]
     regex = /href=["']\(?\bhttp:\/\/[-A-Za-z0-9+&@#\/%?=~_()|!:,.;]*[-A-Za-z0-9+&@#\/%=~_()|]['"]/g;
-    var matches = entity.content.match(regex);
+    matches = entity.content.match(regex);
     if (matches && matches.length) {
       $.each(matches, function(index, match){
+          // TODO - this needs to be tested!
+          //if (s.StartsWith("(") && s.EndsWith(")")) {
+            //return s.Substring(1, s.Length - 2);
+          //}
           var url = match.substring(6, match.length-1);
           replacement = 'onclick="javascript:window.open(\'' + url + '\', \'_blank\', \'location=yes\');"';
           entity.content = entity.content.replace(match, replacement);
